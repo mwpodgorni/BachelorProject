f7028c0dbffe96d54abe2558b40760deb86215ea
mwpodgorni <mwpodgorni@gmail.com>
2020-09-06 20:54:31 +0200

    removing unecessary dependencies & code

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -56,30 +56,8 @@
           </div>
         </div>
         <div class="row">
-          <div class="col text-center px-0" style="height:100%;">
-            <div>
-              <vue-horizontal-list
-                :items="items"
-                :options="{
-                  responsive: [
-                    { end: 576, size: 1 },
-                    { start: 576, end: 768, size: 2 },
-                    { size: 3 },
-                  ],
-                }"
-              >
-                <template v-slot:default="{ item }">
-                  <div>
-                    <div class="aspect r-5-3 border-3 overflow-hidden">
-                      <img class="suggestion-image" :src="item.image" />
-                    </div>
-
-                    <h5>{{ item.title }}</h5>
-                    <p class=" text-ellipsis-2l">{{ item.content }}</p>
-                  </div>
-                </template>
-              </vue-horizontal-list>
-            </div>
+          <div class="col text-center px-0" style="height: 100%">
+            <div></div>
           </div>
         </div>
       </div>
@@ -118,68 +96,11 @@
 import { mapGetters } from "vuex";
 import firebase from "firebase";
 import userData from "@/assets/json-data/user-data.json";
-import VueHorizontalList from "vue-horizontal-list";
 export default {
-  components: { VueHorizontalList },
+  components: {},
   data() {
     return {
       userData: userData,
-      items: [
-        {
-          title: "Sed non ante non cras amet",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non sagittis leo. Vestibulum sit amet metus nec neque dignissim dapibus.",
-          image: "https://picsum.photos/id/1015/600/600",
-        },
-        {
-          title: "Curabitur sit amet nunc",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id mollis erat. Aliquam erat volutpat. Nunc erat lacus, rhoncus nec.",
-          image: "https://picsum.photos/id/1019/600/600",
-        },
-        {
-          title: "Proin pharetra, ante metus",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac diam ac ex efficitur posuere. Pellentesque cursus pellentesque risus, non.",
-          image: "https://picsum.photos/id/1039/600/600",
-        },
-        {
-          title: "Cras pharetra non enim a",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi malesuada varius nibh, a malesuada nisi feugiat eget. Aenean convallis semper.",
-          image: "https://picsum.photos/id/1042/600/600",
-        },
-        {
-          title: "Proin vulputate, augue eu accumsan",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla tempor libero sit amet mollis. Praesent quis leo erat. Integer.",
-          image: "https://picsum.photos/id/1044/600/600",
-        },
-        {
-          title: "Maecenas feugiat magna sapien in",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet fringilla ante. Quisque at ipsum non lacus consequat dictum.",
-          image: "https://picsum.photos/id/1057/600/600",
-        },
-        {
-          title: "Donec commodo sed enim at",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu condimentum risus. Praesent dignissim, neque nec pharetra vestibulum, libero odio.",
-          image: "https://picsum.photos/id/1063/600/600",
-        },
-        {
-          title: "In bibendum urna et turpis",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae ante volutpat leo vulputate volutpat et sed ex. Vivamus eu.",
-          image: "https://picsum.photos/id/1076/600/600",
-        },
-        {
-          title: "Phasellus iaculis dignissim erat at",
-          content:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis quam scelerisque, eleifend purus gravida, scelerisque orci. Ut et turpis.",
-          image: "https://picsum.photos/id/1083/600/600",
-        },
-      ],
     };
   },
   methods: {
@@ -219,27 +140,5 @@ export default {
 #recentlyPlayed-list {
   height: 200px;
   overflow: auto;
-}
-
-section > div {
-  margin-top: 24px;
-  margin-bottom: 24px;
-}
-.Banner {
-  background-image: url("https://picsum.photos/id/1011/2000/500");
-}
-.background-image {
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-}
-.suggestion-image {
-  object-fit: cover;
-  width: 50%;
-  height: 50%;
-}
-.ImageBoxed {
-  flex: 0 0 25%;
-  max-width: 25%;
 }
 </style>
